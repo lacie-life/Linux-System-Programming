0b999ece6ae0230e5a6cdceb19ca35fbd87c21bb
lacie-life <00sao00ios00@gmail.com>
2021-03-10 23:41:03 +0700

    FIFO example

---
+++ FIFO/example-1.c
@@ -0,0 +1,34 @@
+// C program to implement one side of FIFO
+// This side writes first, then reads
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    int fd;
+    // FIFO file path
+    char *myfifo = "/tmp/myfifo";
+    char arr2[80];
+
+    // Creating the named file(FIFO)
+    // mkfifo(<pathname>, <permisson>)
+    mkfifo(myfifo, 0660);
+    // Open FIFO for write only
+    fd = open(myfifo, O_WRONLY);
+
+    while (1)
+    {
+        printf("\n Enter string to be sent via fifo \n");
+        fgets(arr2, 80, stdin);
+
+        // Write the input arr2 on FIFO
+        // and close it
+        write(fd, arr2, strlen(arr2)+1); 
+    }
+    close(fd);
+    return 0;
+}
